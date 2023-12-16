2d495c076b0b1ac7d797907c2751557ff20c0dcf
Mathenge-Alex <arxmaina@gmail.com>
2023-12-15 17:36:45 -0800

    Print combinations brains

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * This is the main block
+ * Description: print all single digit, base 100 numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int c = 0;
+
+	while (c < 10)
+	{
+		putchar(48 + c);
+		if (c != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
