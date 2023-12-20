a3597fbf661fb6c3ad90372736d7cb4a7577c85b
Mathenge-Alex <arxmaina@gmail.com>
2023-12-19 18:39:48 -0800

    functiona and loops

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "stdio.h"
+
+/**
+ * main - Main func
+ * _islower - check for lower case letter
+ * @c : character to check the case
+ * Return:0 or 1
+ */
+
+int _islower(int c)
+{
+	return (c >= 97 && c <= 122);
+
+}
