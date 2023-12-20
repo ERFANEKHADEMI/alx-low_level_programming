31e239e0250b934d12f3e2ccca7c786cd6c17521
Mathenge-Alex <arxmaina@gmail.com>
2023-12-19 18:42:22 -0800

    functiona and loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "stdio.h"
+/**
+ * Main - Main func
+ * Description - computes the absolute value for an interger
+ * @n: interger to be tested
+ * Return: 0 or 1
+ */
+
+int _abs(int n)
+
+{
+	return (n < 0 ? -1 * n : n);
+}
