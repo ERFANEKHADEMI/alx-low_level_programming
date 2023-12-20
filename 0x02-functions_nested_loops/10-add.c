5eec8e300add3ffbb059fd7864f61c38cc33f634
Mathenge-Alex <arxmaina@gmail.com>
2023-12-19 18:47:24 -0800

    functiona and loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "stdio.h"
+/**
+ * maina - Main Desc
+ * Description - returns the sum of its parameters
+ * @a: int type number
+ * @b: int type number
+ * Return:0
+ */
+
+
+int add(int a, int b)
+{
+return (a + b);
+}
