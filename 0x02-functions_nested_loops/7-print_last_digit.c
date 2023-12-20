b258ba0e04e39f9e370f959e936e1552762751c7
Mathenge-Alex <arxmaina@gmail.com>
2023-12-19 18:43:58 -0800

    functiona and loops

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "stdio.h"
+/**
+ * Main - Maina Func
+ * Description - return last digit
+ * @n : number to check
+ * Return:0 or 1
+ */
+
+int print_last_digit(int n)
+{
+	int nv;
+
+	if (n < 0)
+		nv = -1 * (n % 10);
+	else
+		nv = n % 10;
+
+	_putchar(nv + '0');
+		return (nv);
+}
