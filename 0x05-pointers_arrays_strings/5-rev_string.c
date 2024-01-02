08d8db1ed2819180c8399bec8c51c3f5164d5ec7
Mathenge-Alex <arxmaina@gmail.com>
2024-01-02 15:19:04 -0800

    Pointers

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * rev_string - Reverses a string
+ * @s: String pointer
+ * Return: return void
+ */
+
+void rev_string(char *s)
+{
+	int t = 0;
+	int k = 0;
+
+	while (s[t])
+	{
+		t++
+	}
+	for (t = t; t >= 0; t--)
+	{
+		s[k] = s[t];
+	}
+}
