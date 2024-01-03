1f59cca612dfc4d2754b881e14d9586392642452
Mathenge-Alex <arxmaina@gmail.com>
2024-01-03 04:37:40 +0300

    Pointers

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+  * main - functions generates passwords
+  * randomly for 101-crackme program
+  * Return: function returns zero
+  */
+int main(void)
+{
+	int summation;
+	char k;
+
+	srand(time(NULL));
+	while (summation <= 2645)
+	{
+		k = rand() % 128;
+		summation += k;
+		putchar(k);
+	}
+	putchar(2772 - summation);
+	return (0);
+}
